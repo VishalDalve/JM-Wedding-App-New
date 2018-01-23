c1aeb4e80b6b7f55c38684c21cd54c6476e6534e
Vishal D <vishal@websvento.com>
2018-01-23 17:12:58 +0530

    animation added

--- app/src/main/java/com/app/weddingjmnew/MainActivity.java
+++ app/src/main/java/com/app/weddingjmnew/MainActivity.java
@@ -2,18 +2,19 @@ package com.app.weddingjmnew;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout groom, bride, maps;
     Intent i;
-
+    ImageView lft, rgt;
     Context mcontext;
 
     @Override
@@ -26,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
         groom = findViewById(R.id.ll_groom);
         bride = findViewById(R.id.ll_bride);
         maps = findViewById(R.id.ll_maps);
+
+        lft = findViewById(R.id.iv_lft);
+        rgt = findViewById(R.id.iv_rgt);
+
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.ltor);
+        rgt.startAnimation(animation);
+        Animation animationn = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.rtof);
+        lft.startAnimation(animationn);
 
 
         groom.setOnClickListener(new View.OnClickListener() {
